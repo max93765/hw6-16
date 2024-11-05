78fbcc1bb86952899825b4d3cc97d2f3d483cd21
max93765 <maxlovesugar@gmail.com>
2024-11-05 22:53:30 +0800

    Add files via upload

---
+++ hw 6-16 extened (not finished).cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void) 
+{
+
+    double a, b, c;
+    double x1,x2;
+    printf("請輸入a b c: ");
+    scanf_s("%f %f %f", &a, &b, &c);
+
+  
+    if (pow(b,2)-4*a*c > 0)
+    {
+        x1 = (-b + sqrt(pow(b,2)-4*a*c))/ 2 * a;
+        x2 = (-b - sqrt(pow(b, 2) - 4 * a * c)) / 2 * a;
+
+        printf("方程式有兩個實根 x = %f 和 %f\n", x1, x2);
+    }
+    else if (pow(b, 2) - 4 * a * c == 0)
+    {
+        x1 == x2= -b / 2 * a;
+
+            printf("方程式有兩相等實根 x = %f\n", x1=x2);
+    }
+            else
+    {
+                printf("沒有實根。\n");
+    }
+    return 0;
+
+}
